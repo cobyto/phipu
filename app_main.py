b69bc5e520df28f59dc4fd9d5fb3c316e2769d6b
cobyto <kobito1592@gmail.com>
2021-05-18 00:20:11 +0900

    fix folder stracture of transcription

--- app_main.py
+++
@@ -1,8 +0,0 @@
-from transcription.nakata_univ import yaritai_koto_no_mitukekata as yknm
-
-def test_やりたいことの見つけ方():
-    "テスト実行用関数"
-    yknm.やりたいことの見つけ方().やりたいことを見つける()
-
-if __name__ == "__main__":
-    test_やりたいことの見つけ方()
